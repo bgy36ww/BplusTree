33e9540fbc4c5b6562c5382e6137ade0c13ffd54
bgy36ww <david36ww@hotmail.com>
2016-03-07 13:04:13 -0500

    Debugged search

---
+++ Teststructure.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Waver
+ */
+public class Teststructure {
+    
+
+    public static void main(String[] args) {
+        int i=0;
+        int k=0;
+        Node tt=new LeafNode(i,k);
+        System.out.println(((LeafNode)tt).values);
+    
+    
+    }    
+}
