9f6e1fa6eb68687e5271531e42bc9359ff437337
CharlesSiqiLiang <liangsq2050@gmail.com>
2016-03-09 14:56:28 -0500

    New Stuff

--- BPlusTree.java
+++ BPlusTree.java
@@ -27,7 +27,7 @@ public class BPlusTree<K extends Comparable<K>, T> {
     public T tree_search(Node<K,T> startNode, K key) {
         // If the starting node is a leafNode
         if (startNode.isLeafNode) {
-        	// Get the position of the key in the list of keys
+        	// Get tshe position of the key in the list of keys
 
         	int position = startNode.keys.indexOf(key);
         	// Get its value from the list of values.
