79324b3d77bcab37058bc32448eeddb0ae2ee345
bgy36ww <david36ww@hotmail.com>
2016-03-11 17:27:23 -0500

    comments added

--- BPlusTree.java
+++ BPlusTree.java
@@ -83,8 +83,10 @@ public class BPlusTree<K extends Comparable<K>, T> {
             while ((i<N.keys.size())&&(key.compareTo((K)N.keys.get(i))>=0)){
                 i++;
             }
+            //check for special case. Since leave node does not have m+1 values.
             if (N.isLeafNode){
             i--;}
+            //create return variable
             Entry<K,Node<K,T>> entry;
             if (!N.isLeafNode){
             //recur through the node
@@ -95,7 +97,7 @@ public class BPlusTree<K extends Comparable<K>, T> {
                 }
                 //check overflow and return
                 if (N.isOverflowed()){
-
+                    //check if it's the root node
                    if (depth!=0){
 
                    entry=this.splitIndexNode((IndexNode)N);
@@ -115,7 +117,11 @@ public class BPlusTree<K extends Comparable<K>, T> {
                 }
              
             }else {
-                   ((LeafNode)N).insertSorted(key, value);
+                    //if reached leaf node
+                    //insert key and value
+                    ((LeafNode)N).insertSorted(key, value);
+                   //check overflow
+                   //return entry if node has been split
                    if (N.isOverflowed()){
                    if (depth!=0){
                         entry=this.splitLeafNode((LeafNode)N);
@@ -143,13 +149,16 @@ public class BPlusTree<K extends Comparable<K>, T> {
 	 */
 	public Entry<K, Node<K,T>> splitLeafNode(LeafNode<K,T> leaf) {
             int n=leaf.keys.size();
+            //create new node
             LeafNode nLeaf=new LeafNode(leaf.keys.subList(D, n),leaf.values.subList(D, n));
-
+            //get the key for parent node
             K tkey=leaf.keys.get(D);
+            //remove excess node
             for (int i=D;i<n;i++){
             leaf.keys.remove(D);
             leaf.values.remove(D);
             }
+            //connect nodes
             if (leaf.nextLeaf != null) {
             leaf.nextLeaf.previousLeaf = nLeaf;
             nLeaf.nextLeaf = leaf.nextLeaf;
@@ -160,6 +169,7 @@ public class BPlusTree<K extends Comparable<K>, T> {
             leaf.nextLeaf=nLeaf;
             nLeaf.previousLeaf=leaf;
             }
+            //create return entry
             Entry<K, Node<K,T>> reentry=new SimpleEntry<K,Node<K,T>>(tkey,nLeaf);
             return reentry;
 	}
@@ -174,15 +184,17 @@ public class BPlusTree<K extends Comparable<K>, T> {
 	public Entry<K, Node<K,T>> splitIndexNode(IndexNode<K,T> index) {
             int n=index.keys.size();
             int m=index.children.size();
- 
+            //create new index node for return
             IndexNode nindex=new IndexNode(index.keys.subList(D+1, n),index.children.subList(D+1, m));
             K tkey=index.keys.get(D);
+            //delete copied node
             for (int i=D;i<n;i++){
             index.keys.remove(D);
             }
             for (int i=D+1;i<m;i++){
             index.children.remove(D+1);
             }
+            //create return node
             Entry<K, Node<K,T>> reentry=new SimpleEntry<K,Node<K,T>>(tkey,nindex);
 
             return reentry;
@@ -194,19 +206,23 @@ public class BPlusTree<K extends Comparable<K>, T> {
 	 * @param key
 	 */
 	public void delete(K key) {
-              
+              //using recursion function here
               recdelete(key,root);
+              //default if no root just do nothing
+              if (root!=null){
               if (root.keys.size()==0){
               	root=(Node)((IndexNode)root).children.get(0);
-              }
+              }}
+              
 	}
         
         public boolean recdelete(K key, Node N){
             int i=0;
+            //find the position to delete
             while ((i<N.keys.size())&&(key.compareTo((K)N.keys.get(i))>=0)){
                 i++;
             }
-            
+            //help on leafnode
             if (N.isLeafNode){
             i--;
             if (key.compareTo((K)N.keys.get(i))==0){
@@ -216,25 +232,32 @@ public class BPlusTree<K extends Comparable<K>, T> {
             }
 
             }else{
+                //go into index node and continue finding the node to delete
             	if (recdelete(key, ((Node)((IndexNode)N).children.get(i)))){
             		Node Secnode;
             		Node Node1=null;
             		Node Node2=null;
+                        //create three nodes for relocation if one node is underflow
             		int size1=-1;
             		int size2=-1;
+                        //obtain left node if and only if there is a left node
             		if (i>0){
             			Node1=((Node)((IndexNode)N).children.get(i-1));
             			size1=Node1.keys.size();
             		}
                         int k=0;
                         if (((Node)(((IndexNode)N).children.get(i))).isLeafNode){k=1;}
-            		if (i<N.keys.size()-k){
+            		//obtain right node if and only if there is a right node, also use k to help positon the pointer if the children is leafnode
+                        if (i<N.keys.size()-k){
             			Node2=((Node)((IndexNode)N).children.get(i+1));
             			size2=Node2.keys.size();
             		}
+                        //determine which node has more nodes to redistribute
             		Secnode=size1>size2?Node1:Node2;
+                        //do leaf redistribute if children is leaf node
             		if (((Node)(((IndexNode)N).children.get(i))).isLeafNode){
             			int pos=-1;
+                                //determine left and right
             			if (size1>size2){
             			pos=this.handleLeafNodeUnderflow((LeafNode)Secnode,(LeafNode)(((IndexNode)N).children.get(i)),(IndexNode)N);
             			}else{
@@ -246,9 +269,10 @@ public class BPlusTree<K extends Comparable<K>, T> {
                                     ((IndexNode)N).keys.remove(pos);
                                 }	
             			}
+                        //do index node redistribute otherwise
             		else{
             			int pos=-1;
-
+                                //determine left and right
             			if (size1>size2){
             			pos=this.handleIndexNodeUnderflow((IndexNode)Secnode,(IndexNode)(((IndexNode)N).children.get(i)),(IndexNode)N);
             			}else{
@@ -264,6 +288,7 @@ public class BPlusTree<K extends Comparable<K>, T> {
             		
             	}
             }
+            //return current node status, if underflow, the parent will do handle
             return N.isUnderflowed();
         }
 
@@ -289,13 +314,15 @@ public class BPlusTree<K extends Comparable<K>, T> {
                 pos++;
                 }
                 pos--;
+                //take record of three nodes properties for later uses
 		int l1=left.values.size();
 		int l2=right.values.size();
 		int p1=parent.keys.size();
+                //if not enough keys on left and right
 		if (l1+l2<2*D){
-
+                    
 			K tkey=parent.keys.get(pos);
-
+                        //pull down and redistribute nodes
 			for (int i=0;i<l2;i++){
 				left.keys.add(right.keys.get(0));
 				right.keys.remove(0);
@@ -307,6 +334,8 @@ public class BPlusTree<K extends Comparable<K>, T> {
 			right=null;
                         return pos;
 		}else{
+                    //if there is enough keys, do redistribution
+                    //detemine direction of the redistribution
                         if (l2>l1){
 			int numtomove=D-l1;
 			for (int i=0;i<numtomove;i++){
@@ -325,7 +354,7 @@ public class BPlusTree<K extends Comparable<K>, T> {
 				left.values.remove(left.values.size()-1);
 			}    
                                 }
-                        
+                        //modified parent node with the correct keys
 			parent.keys.remove(pos);
 			parent.keys.add(pos,right.keys.get(0));
 		}
@@ -357,6 +386,7 @@ public class BPlusTree<K extends Comparable<K>, T> {
                 pos--;
                 }
 		int l1=0;
+                //make sure there exists a node on left or right
                 if (left!=null){
                     l1=left.keys.size();
                 }
@@ -364,7 +394,9 @@ public class BPlusTree<K extends Comparable<K>, T> {
                 if (right!=null){
                     l2=right.keys.size();
                 }
+                //if there is not enough keys on left and right
 		if (l1+l2<2*D){
+                        //move keys from left to right
 			left.keys.add(parent.keys.get(pos));
 			for (int i=0;i<l2;i++){
 				left.keys.add(right.keys.get(0));
@@ -375,10 +407,13 @@ public class BPlusTree<K extends Comparable<K>, T> {
                         if (l2>0){
 			left.children.add(right.children.get(0));
 			right.children.remove(0);}
-			
+			//destory right
 			right=null;
+                        //give parent position to delete the key and children
                         return pos;
 		}else{
+                    //if there is enough keys
+                    //detemine redistribution direction
                         if (l2>l1){
                         left.keys.add(parent.keys.get(pos));
 			int numtomove=D-l1;
@@ -401,6 +436,7 @@ public class BPlusTree<K extends Comparable<K>, T> {
 				right.children.add(0,left.children.get(left.children.size()-1));
 				left.children.remove(left.children.size()-1);
 			}    
+                        //modified parent node with the correct key
                         parent.keys.remove(pos);
 			parent.keys.add(pos,right.keys.get(0));
                         right.keys.remove(0);
